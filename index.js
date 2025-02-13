33f8c6a4ff074d1aa8a49233caf93e16b7f896a7
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-02-13 23:36:28 +0530

    Minor Changes

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ import axios from "axios";
 import dotenv from "dotenv";
 
 const app = express();
-const port = 3000;
+const port = 8000;
 
 app.use(express.static("public"));
 dotenv.config();
 
 app.get("/" , async (req,res)=>{
     try {
-        const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.NEWS_API_KEY}`);
+        const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
         const firstArticle = result.data.articles;
         res.render("index.ejs",{
             articles : firstArticle
