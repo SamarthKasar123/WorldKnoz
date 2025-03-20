25c73d1ebe2546b6de1d043f00c1b1bbfad032cf
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-03-20 22:07:06 +0530

    First Commit Done

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import=dotenv from "dotenv"
 
 const app = express();
 const port = 8000;
-app.use(express.static("public
+app.use(express.static("public:);
+import usestate from "node:worker_threads";
+app.use(bodyParser.urlencoded({extended:true}));
 dotenv.config(
 
 app.get("/" , async (req,res)=>{
@@ -13,7 +15,7 @@ app.get("/" , async (req,res)=>{
         const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
         const firstArticle = result.data.articles;
         res.render("index.ejs",{
-            articles : firstArticle
+            articles : firstArticl
         });
     } catch (error) {
         console.log(error.result.data);
