aebc6650913aca49f3f7ee3022c1e84cb396bf10
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-03-22 22:57:41 +0530

    Small changes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/" , async (req,res)=>{
         const result = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}`);
         const firstArticle = result.data.articles;
         res.render("index.ejs",{
-            articles : firstArtic
+            articles : firstArti
         });
     } catch (error) {
         console.log(error.result.data);
